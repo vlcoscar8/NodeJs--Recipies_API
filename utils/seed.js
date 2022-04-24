500767f3533fe08f75b7a453a1d51bc1759c5f9c
vlcoscar8 <vlcoscar8@gmail.com>
2022-04-24 21:53:15 +0200

    seed updated

--- utils/seed.js
+++ utils/seed.js
@@ -8,52 +8,52 @@ const DB_URL = process.env.DB_URL;
 const foodSeed = [
     {
         name: "Italian Food",
-        img: "https://res.cloudinary.com/dg143woyr/image/upload/v1650728474/RecipeAssets/FoodCategory/italianFood_lkz9uo.jpg",
+        img: "https://res.cloudinary.com/oscar-perez/image/upload/v1650728474/RecipeAssets/FoodCategory/italianFood_lkz9uo.jpg",
         recipes: [],
     },
     {
         name: "Japanese Food",
-        img: "https://res.cloudinary.com/dg143woyr/image/upload/v1650728474/RecipeAssets/FoodCategory/japaneseFood_cfd61m.jpg",
+        img: "https://res.cloudinary.com/oscar-perez/image/upload/v1650728474/RecipeAssets/FoodCategory/japaneseFood_cfd61m.jpg",
         recipes: [],
     },
     {
         name: "Mexican Food",
-        img: "https://res.cloudinary.com/dg143woyr/image/upload/v1650728473/RecipeAssets/FoodCategory/mexicanFood_uzfuzz.jpg",
+        img: "https://res.cloudinary.com/oscar-perez/image/upload/v1650728473/RecipeAssets/FoodCategory/mexicanFood_uzfuzz.jpg",
         recipes: [],
     },
     {
         name: "Spanish Food",
-        img: "https://res.cloudinary.com/dg143woyr/image/upload/v1650728474/RecipeAssets/FoodCategory/spanishFood_ft3fys.jpg",
+        img: "https://res.cloudinary.com/oscar-perez/image/upload/v1650728474/RecipeAssets/FoodCategory/spanishFood_ft3fys.jpg",
         recipes: [],
     },
     {
         name: "Indian Food",
-        img: "https://res.cloudinary.com/dg143woyr/image/upload/v1650728473/RecipeAssets/FoodCategory/indianFood_hotdnf.jpg",
+        img: "https://res.cloudinary.com/oscar-perez/image/upload/v1650728473/RecipeAssets/FoodCategory/indianFood_hotdnf.jpg",
         recipes: [],
     },
     {
         name: "Chinese Food",
-        img: "https://res.cloudinary.com/dg143woyr/image/upload/v1650728473/RecipeAssets/FoodCategory/chineseFood_ueg7hc.jpg",
+        img: "https://res.cloudinary.com/oscar-perez/image/upload/v1650728473/RecipeAssets/FoodCategory/chineseFood_ueg7hc.jpg",
         recipes: [],
     },
     {
         name: "French Food",
-        img: "https://res.cloudinary.com/dg143woyr/image/upload/v1650728473/RecipeAssets/FoodCategory/frenchFood_g8ezjk.jpg",
+        img: "https://res.cloudinary.com/oscar-perez/image/upload/v1650728473/RecipeAssets/FoodCategory/frenchFood_g8ezjk.jpg",
         recipes: [],
     },
     {
         name: "American Food",
-        img: "https://res.cloudinary.com/dg143woyr/image/upload/v1650728472/RecipeAssets/FoodCategory/AmericanFood_rqe4mm.jpg",
+        img: "https://res.cloudinary.com/oscar-perez/image/upload/v1650728472/RecipeAssets/FoodCategory/AmericanFood_rqe4mm.jpg",
         recipes: [],
     },
     {
         name: "Vegan Food",
-        img: "https://res.cloudinary.com/dg143woyr/image/upload/v1650728474/RecipeAssets/FoodCategory/VeganFood_vpt78b.jpg",
+        img: "https://res.cloudinary.com/oscar-perez/image/upload/v1650728474/RecipeAssets/FoodCategory/VeganFood_vpt78b.jpg",
         recipes: [],
     },
     {
         name: "Nordic Food",
-        img: "https://res.cloudinary.com/dg143woyr/image/upload/v1650728475/RecipeAssets/FoodCategory/nordicFood_crwegt.jpg",
+        img: "https://res.cloudinary.com/oscar-perez/image/upload/v1650728475/RecipeAssets/FoodCategory/nordicFood_crwegt.jpg",
         recipes: [],
     },
 ];
