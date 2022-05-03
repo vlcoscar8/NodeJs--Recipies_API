6d24ef1aaf0e65ec99d11d50f93abdf97b117d3f
vlcoscar8 <vlcoscar8@gmail.com>
2022-05-03 12:13:17 +0200

    cors updated

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 server.use(
     cors({
-        origin: `${HEROKU}`,
+        origin: `https://apirecetasoscar.herokuapp.com/`,
     })
 );
 server.use(
