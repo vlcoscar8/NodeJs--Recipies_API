75a35791b6ca2a969942936c9b701c1812cdd2b3
vlcoscar8 <vlcoscar8@gmail.com>
2022-05-03 12:43:16 +0200

    cors

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 server.use(
     cors({
-        origin: `http://localhost:4000`,
+        origin: `*`,
     })
 );
 server.use(
