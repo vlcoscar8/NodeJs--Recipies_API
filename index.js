bc59df46b4f3331d757ba5837c6cf8676df45982
vlcoscar8 <vlcoscar8@gmail.com>
2022-05-15 09:36:28 +0200

    foto imagen home

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ server.all("/", (req, res) => {
     res.send(`
     <div style="display: flex; flex-direction: column; justify-content: center; align-items: center">
         <h1>Recipe social media api done by Oscar Perez</h1>
-        <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/5a28e397-d083-4398-b0d9-a9911ae22018/react-infinity-run-flyknit-3-zapatillas-de-running-carretera-Pp5hlk.png" style="width: 250px"/>
+        <img src="https://www.clara.es/medio/2021/02/11/100-recetas-saludables-con-5-ingredientes-pescado_1eb7cc7f_800x700.jpg" style="width: 250px"/>
         <h2>Documentation:</h2>
         <a href="https://apirecetasoscar.herokuapp.com/api-doc/" target="_blank">Recipe social media api documentation</a>
         <h3>Git hub repository</h3>
